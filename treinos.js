87654e47cdeb08cf57f525ae7bc2428fdb12eaa1
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-06-07 17:33:49 -0300

    adicionei uns programas em string

--- treinos.js
+++ treinos.js
@@ -61,19 +61,62 @@ console.log(multiply())
 
 
 
-function numbersOrdem(...ordemArr){
-    let numerosOrdenados = [];
-    for (let i = 1; i < ordemArr.length; i ++) {
-        for (let j = 0; j < i; j ++) {
-            if (ordemArr[i] < ordemArr[j]) {
-                let position = ordemArr[i];
-                ordemArr[i] = ordemArr[j];
-                ordemArr[j] = position;
-                numerosOrdenados.push(ordemArr[position])
-            }
-        }
-      }
-      return numerosOrdenados;
 
+  ///////////////////////////////////////////////////////////
+
+
+  //Template string.
+
+//   const nome = "Gabriel";
+//   const idade = 26;
+//   const cidade = "Campinas";
+
+//   const newTemp = `Meu nome é ${nome}, tenho ${idade} anos e moro na cidade de ${cidade}`
+
+//   console.log(newTemp)
+
+
+
+//   function numerosAteDez(){
+//       let numbersAte = [];
+//       for(let i = 1; i <= 10; i++){
+//           numbersAte.push(i);
+//       }
+//       return numbersAte
+//   }
+
+//   console.log(`Numeros crescentes de 1 até 10 são: ${numerosAteDez()}`)
+
+
+
+function listaFrutas(...arrFrutas){
+    let soma = 0;
+    for(let i = 0; i < arrFrutas.length; i++){
+        soma += arrFrutas[i]
+    }
+    if(soma > 15){
+        return soma
+        
+    }else{
+        return " valor menor que 16"
+    }
+    
 }
-  console.log(numbersOrdem(5, 9, 3, 19, 70, 8, 100, 2, 35, 27))
+
+console.log(listaFrutas(5,5,10,20))
+
+////////////////////////////////////////
+
+
+// metodo para inverter uma palavra em string
+
+let word = 'Gabriel Coelho';
+let inversao = word.split("").reverse().join("");
+console.log(inversao)
+
+/////////////////////////////////////////////////
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+for(let i = 0; i < array.length; i++){
+    
+}
